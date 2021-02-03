0def25a7e60c9421577cc9bbcc31b48cb081edb3
Vlad Beffa <vbeffa@mac.com>
2021-02-02 19:56:04 -0600

    move types, add helper funcs/methods

--- cmd/trueaccord/main.go
+++ cmd/trueaccord/main.go
@@ -8,6 +8,26 @@ import (
 	"net/http"
 )
 
+type debt struct {
+	Id     int     `json:"id"`
+	Amount float32 `json:"amount"`
+}
+
+type paymentPlan struct {
+	Amount               float32 `json:"amount_to_pay"`
+	DebtId               int     `json:"debt_id"`
+	Id                   int     `json:"id"`
+	InstallmentAmount    float32 `json:"installment_amount"`
+	InstallmentFrequency string  `json:"installment_frequency"`
+	StartDate            string  `json:"start_date"`
+}
+
+type payment struct {
+	Amount        float32 `json:"amount"`
+	Date          string  `json:"date"`
+	PaymentPlanId int     `json:"payment_plan_id"`
+}
+
 func main() {
 	resp, err := http.Get("https://my-json-server.typicode.com/druska/trueaccord-mock-payments-api/debts")
 	if err != nil {
@@ -20,11 +40,6 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Println(string(respBody))
-
-	type debt struct {
-		Id     int     `json:"id"`
-		Amount float32 `json:"amount"`
-	}
 
 	var debts []debt
 	if err := json.Unmarshal(respBody, &debts); err != nil {
@@ -44,15 +59,6 @@ func main() {
 	}
 	fmt.Println(string(respBody))
 
-	type paymentPlan struct {
-		Amount               float32 `json:"amount_to_pay"`
-		DebtId               int     `json:"debt_id"`
-		Id                   int     `json:"id"`
-		InstallmentAmount    float32 `json:"installment_amount"`
-		InstallmentFrequency string  `json:"installment_frequency"`
-		StartDate            string  `json:"start_date"`
-	}
-
 	var paymentPlans []paymentPlan
 	if err := json.Unmarshal(respBody, &paymentPlans); err != nil {
 		log.Fatal(err)
@@ -71,15 +77,55 @@ func main() {
 	}
 	fmt.Println(string(respBody))
 
-	type payment struct {
-		Amount        float32 `json:"amount"`
-		Date          string  `json:"date"`
-		PaymentPlanId int     `json:"payment_plan_id"`
-	}
-
 	var payments []payment
 	if err := json.Unmarshal(respBody, &payments); err != nil {
 		log.Fatal(err)
 	}
 	fmt.Printf("%v\n", payments)
+
+	type outputDebt struct {
+		Id                 int     `json:"id"`
+		Amount             float32 `json:"amount"`
+		IsInPaymentPlan    bool    `json:"is_in_payment_plan"`
+		RemainingAmount    float32 `json:"remaining_amount"`
+		NextPaymentDueDate string  `json:"next_payment_due_date"`
+	}
+
+	for _, debt := range debts {
+		fmt.Printf("%v\n", debt)
+
+	}
+}
+
+func (d *debt) isInPaymentPlan(paymentPlans []paymentPlan) bool {
+	return d.findPaymentPlan(paymentPlans) != nil
+}
+
+func (d *debt) findPaymentPlan(paymentPlans []paymentPlan) *paymentPlan {
+	for _, pp := range paymentPlans {
+		if d.Id == pp.DebtId {
+			return &pp
+		}
+	}
+
+	return nil
+}
+
+func findPayments(pp *paymentPlan, allPayments []payment) []payment {
+	var foundPayments []payment
+	for _, p := range allPayments {
+		if pp.Id == p.PaymentPlanId {
+			foundPayments = append(foundPayments, p)
+		}
+	}
+
+	return foundPayments
+}
+
+func (d *debt) remainingAmount(pp *paymentPlan, allPayments []payment) (remainingAmount float32) {
+	remainingAmount = d.Amount
+	for _, p := range findPayments(pp, allPayments) {
+		remainingAmount = remainingAmount - p.Amount
+	}
+	return
 }
